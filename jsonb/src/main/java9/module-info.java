90f6f3c55aea3b55a0a1bb5f2bd8d5adc7832d43
Rob Bygrave <robin.bygrave@gmail.com>
2021-12-09 21:06:00 +1300

    #4 - Make jackson.core an optional dependency of jsonb

--- jsonb/src/main/java9/module-info.java
+++ jsonb/src/main/java9/module-info.java
@@ -4,8 +4,9 @@ module io.avaje.jsonb {
   exports io.avaje.jsonb;
   exports io.avaje.jsonb.spi;
 
-  requires com.fasterxml.jackson.core;
+  requires static com.fasterxml.jackson.core;
 
+  uses io.avaje.jsonb.spi.IOAdapterFactory;
   uses io.avaje.jsonb.spi.Bootstrap;
   uses io.avaje.jsonb.Jsonb.Component;
 
