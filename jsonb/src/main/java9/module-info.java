a5701dfc87e38fd1f055d21a66c279b9bf721f7a
Rob Bygrave <robin.bygrave@gmail.com>
2022-02-24 17:49:37 +1300

    #15 - ServiceConfigurationError: io.avaje.jsonb.spi.AdapterFactory: module io.avaje.jsonb does not declare uses

--- jsonb/src/main/java9/module-info.java
+++ jsonb/src/main/java9/module-info.java
@@ -4,7 +4,7 @@ module io.avaje.jsonb {
   exports io.avaje.jsonb;
   exports io.avaje.jsonb.spi;
 
-  uses io.avaje.jsonb.spi.IOAdapterFactory;
+  uses io.avaje.jsonb.spi.AdapterFactory;
   uses io.avaje.jsonb.spi.Bootstrap;
   uses io.avaje.jsonb.Jsonb.Component;
 
