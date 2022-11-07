16adbad602248c2b6f06a8e49e3d79002d33242b
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-11-06 20:24:40 -0500

    Update Recycle.java

--- jsonb/src/main/java/io/avaje/jsonb/stream/Recycle.java
+++ jsonb/src/main/java/io/avaje/jsonb/stream/Recycle.java
@@ -1,38 +1,62 @@
 package io.avaje.jsonb.stream;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.Executors;
 
 final class Recycle {
+  private Recycle() {}
 
-  static ThreadLocal<JGenerator> managed = ThreadLocal.withInitial(() -> new JGenerator(4096));
+  private static boolean virtualThreads;
+  private static ThreadLocal<JGenerator> managed;
+  private static ThreadLocal<JParser> read;
 
-  static ThreadLocal<JParser> read = ThreadLocal.withInitial(() -> {
-    char[] ch = new char[4096];
-    byte[] by = new byte[4096];
-    return new JParser(ch, by, 0, JParser.ErrorInfo.MINIMAL, JParser.DoublePrecision.DEFAULT, JParser.UnknownNumberParsing.BIGDECIMAL, 100, 50_000);
-  });
-
-  /**
-   * Return a recycled generator with the given target OutputStream.
-   */
-  static JsonGenerator generator(OutputStream target) {
-    return managed.get().prepare(target);
+  static {
+    try {
+      Executors.class.getMethod("newVirtualThreadPerTaskExecutor").invoke(Executors.class);
+      virtualThreads = true;
+    } catch (final Exception e) {
+      virtualThreads = false;
+    }
+    if (!virtualThreads) {
+      managed = ThreadLocal.withInitial(Recycle::getGenerator);
+      read = ThreadLocal.withInitial(Recycle::getParser);
+    }
   }
 
-  /**
-   * Return a recycled generator with expected "to String" result.
-   */
+  /** Return a recycled generator with the given target OutputStream. */
+  static JsonGenerator generator(OutputStream target) {
+    return (virtualThreads ? getGenerator() : managed.get()).prepare(target);
+  }
+
+  /** Return a recycled generator with expected "to String" result. */
   static JsonGenerator generator() {
-    return managed.get().prepare(null);
+    return (virtualThreads ? getGenerator() : managed.get()).prepare(null);
   }
 
   static JsonParser parser(byte[] bytes) {
-    return read.get().process(bytes, bytes.length);
+    return (virtualThreads ? getParser() : read.get()).process(bytes, bytes.length);
   }
 
-  static JsonParser parser(InputStream in) throws IOException {
-    return read.get().process(in);
+  static JsonParser parser(InputStream in) {
+    return (virtualThreads ? getParser() : read.get()).process(in);
+  }
+
+  static JGenerator getGenerator() {
+    return new JGenerator(4096);
+  }
+
+  static JParser getParser() {
+    final char[] ch = new char[4096];
+    final byte[] by = new byte[4096];
+    return new JParser(
+        ch,
+        by,
+        0,
+        JParser.ErrorInfo.MINIMAL,
+        JParser.DoublePrecision.DEFAULT,
+        JParser.UnknownNumberParsing.BIGDECIMAL,
+        100,
+        50_000);
   }
 }
