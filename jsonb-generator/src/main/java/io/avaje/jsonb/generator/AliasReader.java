51e8dfe4726fbfc51ec3f2a82f987aa1cf6c8cf2
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-12-14 16:10:15 -0500

    add Json Alias annotation

---
+++ jsonb-generator/src/main/java/io/avaje/jsonb/generator/AliasReader.java
@@ -0,0 +1,26 @@
+package io.avaje.jsonb.generator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+
+class AliasReader {
+
+  private static final String JSON_ALIAS = "io.avaje.jsonb.Json.JsonAlias";
+
+  /** Read the Json.Property annotation using annotation mirrors. */
+  static List<String> getAliases(Element element) {
+    for (final AnnotationMirror mirror : element.getAnnotationMirrors()) {
+      if (JSON_ALIAS.equals(mirror.getAnnotationType().toString())) {
+        return mirror.getElementValues().values().stream()
+            .flatMap(v -> ((List<?>) v.getValue()).stream())
+            .map(Object::toString)
+            .map(Util::trimQuotes)
+            .collect(Collectors.toList());
+      }
+    }
+    return null;
+  }
+}
