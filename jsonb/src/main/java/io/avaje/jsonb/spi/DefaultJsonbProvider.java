1a224582f93af6c751b08deb235d5bbbe64e0c8c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-03-16 03:51:39 -0400

    Configure Inject Plugin based on Props (#88)
    * prism 1.6
    
    * use enum switch
    
    * enhanced switch case
    
    * throw exception on duplicate enum value
    
    * skip iteration if static maps are filled
    
    * Update EnumReader.java
    
    * use configs
    
    * Update DefaultJsonbProvider.java
    
    * Update DefaultJsonbProvider.java
    
    * Update DefaultJsonbProvider.java
    
    * Revert "Update DefaultJsonbProvider.java"
    
    This reverts commit afdd3a8d71fb2aa2c9aa75bff9dbd55e037ebbeb.

--- jsonb/src/main/java/io/avaje/jsonb/spi/DefaultJsonbProvider.java
+++ jsonb/src/main/java/io/avaje/jsonb/spi/DefaultJsonbProvider.java
@@ -3,18 +3,28 @@ package io.avaje.jsonb.spi;
 import io.avaje.inject.BeanScopeBuilder;
 import io.avaje.jsonb.Jsonb;
 
-/**
- * Plugin for avaje inject that provides a default Jsonb instance.
- */
+/** Plugin for avaje inject that provides a default Jsonb instance. */
 public final class DefaultJsonbProvider implements io.avaje.inject.spi.Plugin {
 
   @Override
   public Class<?>[] provides() {
-    return new Class<?>[]{Jsonb.class};
+    return new Class<?>[] {Jsonb.class};
   }
 
   @Override
   public void apply(BeanScopeBuilder builder) {
-    builder.provideDefault(null, Jsonb.class, Jsonb.builder()::build);
+    builder.provideDefault(
+        null,
+        Jsonb.class,
+        () -> {
+          var props = builder.propertyPlugin();
+
+          return Jsonb.builder()
+              .failOnUnknown(props.equalTo("jsonb.deserialize.failOnUnknown", "true"))
+              .mathTypesAsString(props.equalTo("jsonb.serialize.mathTypesAsString", "true"))
+              .serializeEmpty(props.notEqualTo("jsonb.serialize.empty", "false"))
+              .serializeNulls(props.equalTo("jsonb.serialize.nulls", "true"))
+              .build();
+        });
   }
 }
