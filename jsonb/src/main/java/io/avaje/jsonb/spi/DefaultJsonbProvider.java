a62cb8fed5d17c80add11da5af8fcbf48eed6737
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-03-16 00:49:27 -0400

    Revert "Update DefaultJsonbProvider.java"
    This reverts commit afdd3a8d71fb2aa2c9aa75bff9dbd55e037ebbeb.

--- jsonb/src/main/java/io/avaje/jsonb/spi/DefaultJsonbProvider.java
+++ jsonb/src/main/java/io/avaje/jsonb/spi/DefaultJsonbProvider.java
@@ -14,6 +14,7 @@ public final class DefaultJsonbProvider implements io.avaje.inject.spi.Plugin {
   @Override
   public void apply(BeanScopeBuilder builder) {
     builder.provideDefault(
+        null,
         Jsonb.class,
         () -> {
           var props = builder.propertyPlugin();
