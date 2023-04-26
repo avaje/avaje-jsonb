d0b9afac95d2db78e18681717808972256132dcf
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-04-26 19:51:20 -0400

    Update module-info.java

--- jsonb-inject-plugin/src/main/java/module-info.java
+++ jsonb-inject-plugin/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module io.avaje.jsonb.plugin {
 
-  requires static io.avaje.jsonb;
+  requires io.avaje.jsonb;
 
-  requires static io.avaje.inject;
+  requires io.avaje.inject;
 
   provides io.avaje.inject.spi.Plugin with io.avaje.jsonb.inject.DefaultJsonbProvider;
 }
