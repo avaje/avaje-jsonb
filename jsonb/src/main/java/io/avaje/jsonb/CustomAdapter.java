9d359c48d5c5196bf188d70bdeb3a971665469f1
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-05-28 20:27:28 -0400

    javadoc

--- jsonb/src/main/java/io/avaje/jsonb/CustomAdapter.java
+++ jsonb/src/main/java/io/avaje/jsonb/CustomAdapter.java
@@ -4,6 +4,7 @@ import static java.lang.annotation.ElementType.TYPE;
 
 import java.lang.annotation.Target;
 
+
 /**
  * Marks a type as a basic user-provided JsonAdapter to be registered automatically.
  *
@@ -20,6 +21,31 @@ import java.lang.annotation.Target;
  *   public CustomJsonAdapter(Jsonb jsonb) {
  *     //use the jsonb adapter method to get type serializers
  *     stringJsonAdapter = jsonb.adapter(String.class);
+ *     //add serialization names
+ *     jsonb.properties("prop1","prop2", ...);
+ *   }
+ * ...
+ *
+ * }</pre>
+ *
+ * <h3>Example of Generic Adapter:</h3>
+ *
+ * <pre>{@code
+ * @CustomAdapter(isGeneric=true)
+ * public class CustomJsonAdapter<T> implements JsonAdapter<GenericType<T>> {
+ *   private final JsonAdapter<T> genericTypeAdapter;
+ *   private final PropertyNames names;
+ *   public static final JsonAdapter.Factory FACTORY =
+ *     (type, jsonb) -> {
+ *       if (type instanceof ParameterizedType && Types.rawType(type) == GenericType.class) {
+ *
+ *          return new CustomJsonAdapter<>(jsonb, Types.typeArguments(type));
+ *        }
+ *        return null;
+ *      };
+ *   public CustomJsonAdapter(Jsonb jsonb, Type[] types) {
+ *     //use the jsonb adapter method to get type serializers
+ *     genericTypeAdapter = jsonb.adapter(types[0]);
  *     //add serialization names
  *     jsonb.properties("prop1","prop2", ...);
  *   }
