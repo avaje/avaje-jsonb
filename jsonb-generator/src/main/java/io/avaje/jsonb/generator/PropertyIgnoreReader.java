ea041ede6df3536b66c36662706e08a19c0e8480
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-03-13 18:24:16 -0400

    simplify record detection

--- jsonb-generator/src/main/java/io/avaje/jsonb/generator/PropertyIgnoreReader.java
+++ jsonb-generator/src/main/java/io/avaje/jsonb/generator/PropertyIgnoreReader.java
@@ -1,36 +1,30 @@
 package io.avaje.jsonb.generator;
 
+import java.util.function.Predicate;
+
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.ElementFilter;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-
-import static java.util.function.Predicate.not;
-import static java.util.stream.Collectors.toSet;
+import javax.lang.model.util.Elements;
 
 final class PropertyIgnoreReader {
 
   private boolean ignoreSerialize;
   private boolean ignoreDeserialize;
-  private static Function<Iterable<? extends Element>, Set<ExecutableElement>> getRecordMethods;
+  private static Predicate<ExecutableElement> isNotRecordAccessor;
 
   PropertyIgnoreReader(Element element, String propertyName) {
     final var enclosingElements = element.getEnclosingElement().getEnclosedElements();
 
-    var recordMethods = getRecordMethods.apply(enclosingElements);
     boolean propertyMethodOverride =
-      element instanceof VariableElement
-        && ElementFilter.methodsIn(enclosingElements).stream()
-        .filter(PropertyPrism::isPresent)
-        .filter(not(recordMethods::contains))
-        .map(PropertyPrism::getInstanceOn)
-        .map(PropertyPrism::value)
-        .anyMatch(propertyName::equals);
+        element instanceof VariableElement
+            && ElementFilter.methodsIn(enclosingElements).stream()
+                .filter(PropertyPrism::isPresent)
+                .filter(isNotRecordAccessor)
+                .map(PropertyPrism::getInstanceOn)
+                .map(PropertyPrism::value)
+                .anyMatch(propertyName::equals);
 
     ignoreSerialize = propertyMethodOverride;
 
@@ -52,40 +46,20 @@ final class PropertyIgnoreReader {
   // needed to filter out record components from @Json.Property Overriding
   static {
     try {
-      var lookup = MethodHandles.lookup();
-      var recordComponentsMethod =
-        lookup.findStatic(
-          ElementFilter.class,
-          "recordComponentsIn",
-          MethodType.methodType(List.class, Iterable.class));
 
-      Function<Iterable<? extends Element>, List<Object>> recordComponentsIn =
-        e -> {
-          try {
-            return (List<Object>) recordComponentsMethod.invoke(e);
-          } catch (Throwable e1) {
-            throw new IllegalStateException();
-          }
-        };
+      var recordComponentFor =
+          Elements.class.getMethod("recordComponentFor", ExecutableElement.class);
+      isNotRecordAccessor =
+          e -> {
+            try {
+              return recordComponentFor.invoke(APContext.elements(), e) == null;
+            } catch (Exception e1) {
+              return true;
+            }
+          };
 
-      var getAccessor =
-        lookup.findVirtual(
-          Class.forName("javax.lang.model.element.RecordComponentElement"),
-          "getAccessor",
-          MethodType.methodType(ExecutableElement.class));
-
-      getRecordMethods = recordComponentsIn.andThen(l -> l.stream()
-        .map(e -> {
-          try {
-            return getAccessor.invoke(e);
-          } catch (Throwable e1) {
-            throw new IllegalStateException();
-          }
-        })
-        .map(ExecutableElement.class::cast)
-        .collect(toSet()));
     } catch (Exception ex) {
-      getRecordMethods = e -> Set.of();
+      isNotRecordAccessor = e -> true;
     }
   }
 }
