19a24f9b579d648fdc8919bbda305c8f292b36cb
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-06-24 19:21:31 -0400

    remove spi from blackbox

--- blackbox-test/src/main/java/module-info.java
+++ blackbox-test/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module blackbox.test {
 
   requires static io.avaje.jsonb;
   requires java.validation;
-  requires static io.avaje.spi;
 
   provides io.avaje.jsonb.spi.JsonbExtension with org.example.customer.customtype.CustomTypeComponent, org.example.jsonb.GeneratedJsonComponent;
 
