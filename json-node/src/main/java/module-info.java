3eebe33d8dbd0cd64dfadfd155d87a8fabc0632d
Rob Bygrave <robin.bygrave@gmail.com>
2024-12-12 18:13:42 +1300

    [json-node] Make avaje-json-node an optional component for JsonB
    That is, we need to explicitly include avaje-json-node in the classpath/module path.
    
    When JsonB starts with avaje-json-node present, it will register with JsonB thus support the JsonNode types in JsonB.

--- json-node/src/main/java/module-info.java
+++ json-node/src/main/java/module-info.java
@@ -4,4 +4,8 @@ module io.avaje.json.node {
 
   requires transitive org.jspecify;
   requires transitive io.avaje.json;
+
+  requires static io.avaje.jsonb;
+  exports io.avaje.json.node.adapter to io.avaje.jsonb;
+  provides io.avaje.jsonb.spi.JsonbComponent with io.avaje.json.node.adapter.JsonNodeComponent;
 }
