c5755b704111358ea7dfb9938bdfa1f13e7e682d
Rob Bygrave <robin.bygrave@gmail.com>
2024-12-16 19:53:39 +1300

    [json-core] Add SimpleMapper.Type list() and map() methods

--- json-core/src/main/java/module-info.java
+++ json-core/src/main/java/module-info.java
@@ -4,7 +4,7 @@ module io.avaje.json {
   exports io.avaje.json.simple;
   exports io.avaje.json.stream;
   exports io.avaje.json.view;
-  exports io.avaje.json.core to io.avaje.jsonb;
+  exports io.avaje.json.core to io.avaje.jsonb, io.avaje.json.node;
 
   requires static io.helidon.webserver;
 }
--- json-core/src/test/java/io/avaje/json/simple/CustomAdapterTest.java
+++ json-core/src/test/java/io/avaje/json/simple/CustomAdapterTest.java
@@ -5,20 +5,20 @@ import io.avaje.json.JsonReader;
 import io.avaje.json.JsonWriter;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomAdapterTest {
 
   static final SimpleMapper simpleMapper = SimpleMapper.builder().build();
+  static final MyAdapter myAdapter = new MyAdapter(simpleMapper);
+  static final SimpleMapper.Type<MyCustomType> type = simpleMapper.type(myAdapter);
 
   @Test
   void mapUsingCustomAdapter() {
-
-    MyAdapter myAdapter = new MyAdapter(simpleMapper);
-
-    SimpleMapper.Type<MyCustomType> type = simpleMapper.type(myAdapter);
 
     MyCustomType source = new MyCustomType();
     source.foo = "hi";
@@ -31,9 +31,57 @@ class CustomAdapterTest {
     assertThat(fromJson.bar).isEqualTo(source.bar);
   }
 
+  @Test
+  void list() {
+    SimpleMapper.Type<List<MyCustomType>> listType = type.list();
+
+    var v0 = as("a", 1);
+    var v1 = as("b", 2);
+    var list = List.of(v0, v1);
+
+    String asJson = listType.toJson(list);
+
+    List<MyCustomType> fromJson = listType.fromJson(asJson);
+    assertThat(fromJson).isEqualTo(list);
+  }
+
+  @Test
+  void map() {
+    SimpleMapper.Type<Map<String, MyCustomType>> mapType = type.map();
+
+    var v0 = as("a", 1);
+    var v1 = as("b", 2);
+    Map<String, MyCustomType> map = Map.of("one", v0, "two", v1);
+
+    String asJson = mapType.toJson(map);
+
+    var fromJson = mapType.fromJson(asJson);
+    assertThat(fromJson).isEqualTo(map);
+  }
+
+  private MyCustomType as(String foo, int bar) {
+    MyCustomType customType = new MyCustomType();
+    customType.foo = foo;
+    customType.bar = bar;
+    return customType;
+  }
+
   static class MyCustomType {
     public String foo;
     public int bar;
+
+    @Override
+    public boolean equals(Object object) {
+      if (this == object) return true;
+      if (!(object instanceof MyCustomType)) return false;
+      MyCustomType that = (MyCustomType) object;
+      return bar == that.bar && Objects.equals(foo, that.foo);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(foo, bar);
+    }
   }
 
   static class MyAdapter implements JsonAdapter<MyCustomType> {
