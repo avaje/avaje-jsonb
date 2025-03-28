d7dd584775ce3c87fc045a196f1f6eb8e683e38d
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-03-28 00:38:59 -0400

    add failOnNullPrimitives plugin config

--- jsonb-inject-plugin/src/main/java/io/avaje/jsonb/inject/DefaultJsonbProvider.java
+++ jsonb-inject-plugin/src/main/java/io/avaje/jsonb/inject/DefaultJsonbProvider.java
@@ -4,8 +4,8 @@ import java.lang.reflect.Type;
 
 import io.avaje.inject.BeanScopeBuilder;
 import io.avaje.inject.spi.InjectPlugin;
-import io.avaje.jsonb.Jsonb;
 import io.avaje.json.stream.BufferRecycleStrategy;
+import io.avaje.jsonb.Jsonb;
 
 /**
  * Plugin for avaje inject that provides a default Jsonb instance.
@@ -19,20 +19,24 @@ public final class DefaultJsonbProvider implements InjectPlugin {
 
   @Override
   public void apply(BeanScopeBuilder builder) {
-    builder.provideDefault(null, Jsonb.class, () -> {
-      var props = builder.configPlugin();
+    builder.provideDefault(
+        null,
+        Jsonb.class,
+        () -> {
+          var props = builder.configPlugin();
 
-      return Jsonb.builder()
-        .failOnUnknown(props.equalTo("jsonb.deserialize.failOnUnknown", "true"))
-        .mathTypesAsString(props.equalTo("jsonb.serialize.mathTypesAsString", "true"))
-        .serializeEmpty(props.notEqualTo("jsonb.serialize.empty", "false"))
-        .serializeNulls(props.equalTo("jsonb.serialize.nulls", "true"))
-        .bufferRecycling(
-          props
-            .get("jsonb.bufferRecycling")
-            .map(BufferRecycleStrategy::valueOf)
-            .orElse(BufferRecycleStrategy.HYBRID_POOL))
-        .build();
-    });
+          return Jsonb.builder()
+              .failOnUnknown(props.equalTo("jsonb.deserialize.failOnUnknown", "true"))
+              .failOnNullPrimitives(props.equalTo("jsonb.deserialize.failOnNullPrimitives", "true"))
+              .mathTypesAsString(props.equalTo("jsonb.serialize.mathTypesAsString", "true"))
+              .serializeEmpty(props.notEqualTo("jsonb.serialize.empty", "false"))
+              .serializeNulls(props.equalTo("jsonb.serialize.nulls", "true"))
+              .bufferRecycling(
+                  props
+                      .get("jsonb.bufferRecycling")
+                      .map(BufferRecycleStrategy::valueOf)
+                      .orElse(BufferRecycleStrategy.HYBRID_POOL))
+              .build();
+        });
   }
 }
