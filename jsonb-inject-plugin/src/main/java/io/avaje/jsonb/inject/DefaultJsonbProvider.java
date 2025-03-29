e4b0c873fd23b16cff9574c4581092e1a487e1f2
Rob Bygrave <robin.bygrave@gmail.com>
2025-03-29 14:50:48 +1300

    Format only

--- jsonb-inject-plugin/src/main/java/io/avaje/jsonb/inject/DefaultJsonbProvider.java
+++ jsonb-inject-plugin/src/main/java/io/avaje/jsonb/inject/DefaultJsonbProvider.java
@@ -19,24 +19,21 @@ public final class DefaultJsonbProvider implements InjectPlugin {
 
   @Override
   public void apply(BeanScopeBuilder builder) {
-    builder.provideDefault(
-        null,
-        Jsonb.class,
-        () -> {
-          var props = builder.configPlugin();
+    builder.provideDefault(null, Jsonb.class, () -> {
+      var props = builder.configPlugin();
 
-          return Jsonb.builder()
-              .failOnUnknown(props.equalTo("jsonb.deserialize.failOnUnknown", "true"))
-              .failOnNullPrimitives(props.equalTo("jsonb.deserialize.failOnNullPrimitives", "true"))
-              .mathTypesAsString(props.equalTo("jsonb.serialize.mathTypesAsString", "true"))
-              .serializeEmpty(props.notEqualTo("jsonb.serialize.empty", "false"))
-              .serializeNulls(props.equalTo("jsonb.serialize.nulls", "true"))
-              .bufferRecycling(
-                  props
-                      .get("jsonb.bufferRecycling")
-                      .map(BufferRecycleStrategy::valueOf)
-                      .orElse(BufferRecycleStrategy.HYBRID_POOL))
-              .build();
-        });
+      return Jsonb.builder()
+        .failOnUnknown(props.equalTo("jsonb.deserialize.failOnUnknown", "true"))
+        .failOnNullPrimitives(props.equalTo("jsonb.deserialize.failOnNullPrimitives", "true"))
+        .mathTypesAsString(props.equalTo("jsonb.serialize.mathTypesAsString", "true"))
+        .serializeEmpty(props.notEqualTo("jsonb.serialize.empty", "false"))
+        .serializeNulls(props.equalTo("jsonb.serialize.nulls", "true"))
+        .bufferRecycling(
+          props
+            .get("jsonb.bufferRecycling")
+            .map(BufferRecycleStrategy::valueOf)
+            .orElse(BufferRecycleStrategy.HYBRID_POOL))
+        .build();
+    });
   }
 }
