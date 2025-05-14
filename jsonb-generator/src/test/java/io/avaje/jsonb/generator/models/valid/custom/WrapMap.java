7de0472ab661794e81358ac0d4851b1ca2cd2cdb
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-05-13 22:41:44 -0400

    Fix custom type adapters with private fields

---
+++ jsonb-generator/src/test/java/io/avaje/jsonb/generator/models/valid/custom/WrapMap.java
@@ -0,0 +1,18 @@
+package io.avaje.jsonb.generator.models.valid.custom;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+public class WrapMap extends AbstractMap<String, String> {
+  private final Map<String, String> delegate;
+
+  public WrapMap(Map<String, String> map) {
+    delegate = map;
+  }
+
+  @Override
+  public Set<Entry<String, String>> entrySet() {
+    return delegate.entrySet();
+  }
+}
---
+++ jsonb-generator/src/test/java/io/avaje/jsonb/generator/models/valid/custom/WrapMap2.java
@@ -0,0 +1,18 @@
+package io.avaje.jsonb.generator.models.valid.custom;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+public class WrapMap2 extends AbstractMap<String, String> {
+  private final Map<String, String> delegate;
+
+  public WrapMap2(Map<String, String> map) {
+    delegate = map;
+  }
+
+  @Override
+  public Set<Entry<String, String>> entrySet() {
+    return delegate.entrySet();
+  }
+}
