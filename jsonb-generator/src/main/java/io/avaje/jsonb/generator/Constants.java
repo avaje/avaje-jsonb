f198e721ba1a2fa87ec60149557c1015c1b007fd
Mechite <contact@mechite.com>
2025-09-21 01:06:35 +0100

    Revert 4355ac4 & e6dc9f2 (#427)
    Signed-off-by: Mechite <contact@mechite.com>

--- jsonb-generator/src/main/java/io/avaje/jsonb/generator/Constants.java
+++ jsonb-generator/src/main/java/io/avaje/jsonb/generator/Constants.java
@@ -3,13 +3,11 @@ package io.avaje.jsonb.generator;
 final class Constants {
 
   static final String META_INF_COMPONENT = "META-INF/services/io.avaje.jsonb.spi.JsonbExtension";
-
   static final String JSONB = "io.avaje.jsonb.Jsonb";
-  static final String JSON = JsonPrism.PRISM_TYPE;
-  static final String JSON_IMPORT = ImportPrism.PRISM_TYPE;
-  static final String JSON_IMPORT_LIST = ImportsPrism.PRISM_TYPE;
-  static final String JSON_MIXIN = MixInPrism.PRISM_TYPE;
-
+  static final String JSON = "io.avaje.jsonb.Json";
+  static final String JSON_IMPORT = "io.avaje.jsonb.Json.Import";
+  static final String JSON_IMPORT_LIST = "io.avaje.jsonb.Json.Import.Imports";
+  static final String JSON_MIXIN = "io.avaje.jsonb.Json.MixIn";
   static final String IOEXCEPTION = "java.io.IOException";
   static final String METHODHANDLE = "java.lang.invoke.MethodHandle";
   static final String REFLECT_TYPE = "java.lang.reflect.Type";
