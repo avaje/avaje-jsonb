2d29a25fc3a0b2c39559405e9c61c8ccabdaea44
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-10-11 21:41:43 -0400

    fix jackson SPI

--- jsonb-jackson/src/main/java/module-info.java
+++ jsonb-jackson/src/main/java/module-info.java
@@ -2,8 +2,8 @@ module io.avaje.jsonb.jackson {
 
   requires transitive io.avaje.jsonb;
   requires transitive com.fasterxml.jackson.core;
+  requires static io.avaje.spi;
 
   exports io.avaje.jsonb.jackson;
-
-  provides io.avaje.jsonb.spi.JsonStreamFactory with io.avaje.jsonb.jackson.JacksonAdapterFactory;
+  provides io.avaje.jsonb.spi.JsonbExtension with io.avaje.jsonb.jackson.JacksonAdapterFactory;
 }
