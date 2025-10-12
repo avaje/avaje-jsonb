f4f9477a8c4d45f7226fed6d1a192bbeb80abfb4
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-10-11 21:49:34 -0400

    test

--- jsonb-jackson/src/test/java/io/avaje/jsonb/jackson/ObjectAdapterTest.java
+++ jsonb-jackson/src/test/java/io/avaje/jsonb/jackson/ObjectAdapterTest.java
@@ -1,25 +1,33 @@
 package io.avaje.jsonb.jackson;
 
-import io.avaje.json.JsonAdapter;
-import io.avaje.json.JsonWriter;
-import io.avaje.jsonb.JsonType;
-import io.avaje.jsonb.Jsonb;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import io.avaje.json.JsonAdapter;
+import io.avaje.json.JsonWriter;
+import io.avaje.jsonb.JsonType;
+import io.avaje.jsonb.Jsonb;
 
 class ObjectAdapterTest {
 
-  Jsonb jsonb = Jsonb.builder().adapter(new JacksonAdapter()).build();
+  Jsonb jsonb = Jsonb.instance();
 
   JsonAdapter<Object> objectAdapter = jsonb.adapter(Object.class);
 
   JsonType<Object> objectType = jsonb.type(Object.class);
+
+  @Test
+  void isJackson() {
+    assertThat(jsonb.reader("")).isInstanceOf(JacksonReader.class);
+    assertThat(jsonb.writer(OutputStream.nullOutputStream())).isInstanceOf(JacksonWriter.class);
+  }
 
   @SuppressWarnings("unchecked")
   @Test
