bbe1ed2706541dfe70ad80294b6e0c6416f7f8ce
Rob Bygrave <robin.bygrave@gmail.com>
2025-12-04 21:38:29 +1300

    ThreadFunctions methods can be package private

--- json-core/src/main/java/io/avaje/json/stream/core/ThreadFunctions.java
+++ json-core/src/main/java/io/avaje/json/stream/core/ThreadFunctions.java
@@ -3,11 +3,11 @@ package io.avaje.json.stream.core;
 final class ThreadFunctions {
   private ThreadFunctions() {}
 
-  public static long getId() {
+  static long getId() {
     return Thread.currentThread().getId();
   }
 
-  public static boolean isVirtual() {
+  static boolean isVirtual() {
     return false;
   }
 }
--- json-core/src/main/java21/io/avaje/json/stream/core/ThreadFunctions.java
+++ json-core/src/main/java21/io/avaje/json/stream/core/ThreadFunctions.java
@@ -3,11 +3,11 @@ package io.avaje.json.stream.core;
 final class ThreadFunctions {
   private ThreadFunctions() {}
 
-  public static long getId() {
+  static long getId() {
     return Thread.currentThread().threadId();
   }
 
-  public static boolean isVirtual() {
+  static boolean isVirtual() {
     return Thread.currentThread().isVirtual();
   }
 }
