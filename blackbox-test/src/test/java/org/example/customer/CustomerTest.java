9ed78de1cb4f17ab1800d0a4a3263db569b6bbfc
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2026-05-25 19:18:28 -0400

    Switch from File to Path

--- blackbox-test/src/test/java/org/example/customer/CustomerTest.java
+++ blackbox-test/src/test/java/org/example/customer/CustomerTest.java
@@ -6,9 +6,10 @@ import io.avaje.jsonb.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Instant;
 import java.util.*;
 
@@ -72,15 +73,15 @@ class CustomerTest {
   @Test
   void anyToFile() throws Exception {
     var customer = new Customer().id(42L).name("rob").status(Customer.Status.ACTIVE);
-    File file = File.createTempFile("customer", ".json");
+    Path path = Files.createTempFile("customer", ".json");
     try {
-      jsonb.toJson(customer, file);
-      Customer fromFile = jsonb.type(Customer.class).fromJson(file);
+      jsonb.toJson(customer, path);
+      Customer fromFile = jsonb.type(Customer.class).fromJson(path);
       assertThat(fromFile.id()).isEqualTo(42L);
       assertThat(fromFile.name()).isEqualTo("rob");
       assertThat(fromFile.status()).isEqualTo(Customer.Status.ACTIVE);
     } finally {
-      file.delete();
+      Files.delete(path);
     }
   }
 
@@ -156,16 +157,16 @@ class CustomerTest {
   void toJson_file() throws Exception {
     Customer customer = customer();
     JsonType<Customer> customerType = jsonb.type(Customer.class);
-    File file = File.createTempFile("customer", ".json");
+    Path path = Files.createTempFile("customer", ".json");
     try {
-      customerType.toJson(customer, file);
-      Customer fromFile = customerType.fromJson(file);
+      customerType.toJson(customer, path);
+      Customer fromFile = customerType.fromJson(path);
       assertThat(fromFile.id()).isEqualTo(customer.id());
       assertThat(fromFile.name()).isEqualTo(customer.name());
       assertThat(fromFile.status()).isEqualTo(customer.status());
       assertThat(fromFile.whenCreated()).isEqualTo(customer.whenCreated());
     } finally {
-      file.delete();
+      Files.delete(path);
     }
   }
 
